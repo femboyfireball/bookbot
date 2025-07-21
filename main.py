794286a2b3b54cba682b157b4ab89795eb9c4afe
femboyfireball <111195856+femboyfireball@users.noreply.github.com>
2025-07-21 15:35:18 -0500

    Add get_book_contents function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_contents(book_path):
+    with open(book_path) as f:
+        book_contents = f.read()
+        return str(book_contents)
+
+def main():
+    book_contents = get_book_contents("books/frankenstein.txt")
+    print(book_contents)
+
+main()
