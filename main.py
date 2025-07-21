19c35ea3f93b9475ecc1b340fb7cd3ea0cfceac9
femboyfireball <111195856+femboyfireball@users.noreply.github.com>
2025-07-21 16:09:12 -0500

    Format the output

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from stats import get_word_count, get_characters_count
 
+
+def sort_on(dictionary):
+    return dictionary["Count"]
+
 def get_book_contents(book_path):
     with open(book_path) as f:
         book_contents = f.read()
         return str(book_contents)
 
 def main():
-    book_contents = get_book_contents("books/frankenstein.txt")
-    word_count = get_word_count(book_contents)
-    print(f"{word_count} words found in the document")
-    print(get_characters_count(book_contents.lower()))
 
+    book = "books/frankenstein.txt"
+    book_contents = get_book_contents(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}")
+
+    word_count = get_word_count(book_contents)
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+    character_counts = get_characters_count(book_contents.lower())
+    character_counts_list = []
+    for character in character_counts:
+        character_dict_form = {
+            "Character" : character,
+            "Count" : character_counts[character]
+        }
+        character_counts_list.append(character_dict_form)
+
+    character_counts_list.sort(reverse=True, key=sort_on)
+    print("--------- Character Count -------")
+    for character in character_counts_list:
+        if character["Character"] == "\n":
+            print(f"\\n: {character["Count"]}")
+        else:
+            print(f"{character["Character"]}: {character["Count"]}")
 main()
